75f3de6da77cbf62c9cbc7f058a78df6a9bb2489
SalmanDelPino <mrsalmandp@gmail.com>
2026-07-20 00:11:35 +0200

    Chenged hello message

--- src/main/java/solomon/pino/Main.java
+++ src/main/java/solomon/pino/Main.java
@@ -3,9 +3,13 @@ package solomon.pino;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
+    private static String msg = "Hello World!";
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.println("nevermore!");
+        System.out.println(msg);
+        System.out.println("Hello1!");
+        System.out.println("Hello2!");
+        System.out.println("Hello3!");
+
+
     }
 }
